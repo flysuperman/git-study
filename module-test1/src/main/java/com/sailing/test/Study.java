a8e8146fc0bbf412f1b6146007d148b9056d4157
rdman <rdman84@126.com.com>
2019-05-22 00:48:02 +0800

    增加study类

---
+++ module-test1/src/main/java/com/sailing/test/Study.java
@@ -0,0 +1,13 @@
+package com.sailing.test;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/5/22 00:47
+ * @Description:
+ */
+public class Study {
+
+    public static void main(String[] args) {
+        System.out.println("study");
+    }
+}
