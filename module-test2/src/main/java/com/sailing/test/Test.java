fddb2377f4e45bbb727f21cecfe0634090a7c0fc
rdman <rdman84@126.com.com>
2019-05-22 00:42:48 +0800

    初次提交

---
+++ module-test2/src/main/java/com/sailing/test/Test.java
@@ -0,0 +1,13 @@
+package com.sailing.test;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/5/22 00:27
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+}
