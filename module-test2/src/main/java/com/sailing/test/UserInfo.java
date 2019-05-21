8dee4121b3a9b939fa76531804033ec819b117de
rdman <rdman84@126.com.com>
2019-05-22 01:34:14 +0800

    增加study类

---
+++ module-test2/src/main/java/com/sailing/test/UserInfo.java
@@ -0,0 +1,13 @@
+package com.sailing.test;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/5/22 01:30
+ * @Description:
+ */
+public class UserInfo {
+
+    private String user;
+
+    private String passWord;
+}
